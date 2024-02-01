a95c03ebbe229ffe2bb68220255e4ddc64cacd2a
AyushiR0y <132144705+AyushiR0y@users.noreply.github.com>
2024-02-01 23:16:53 +0530

    ternary operator in conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-
+//if else
 const x=20;
 if(x===10){
     console.log('x is 10');
@@ -6,3 +6,7 @@ if(x===10){
 else{
     console.log('x is not 10');
 }
+//ternary
+const y=10;
+const color= y>10 ? 'red' : "blue";
+console.log(color);
