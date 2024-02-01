051381e18d3f73673c9d04bb46ba02beb879314b
AyushiR0y <132144705+AyushiR0y@users.noreply.github.com>
2024-02-01 22:19:16 +0530

    Create todos.js

---
+++ todos.js
@@ -0,0 +1,17 @@
+const todos =[
+    {
+        id:1,
+        text: 'take trash out',
+        isCompleted: true
+    },
+    {
+        id:2,
+        text: 'homework done out',
+        isCompleted: false
+    },    {
+        id:3,
+        text: 'learn js',
+        isCompleted: true
+    }
+];
+console.log(todos[1].text);
