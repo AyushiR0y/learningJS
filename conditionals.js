52b85b853ce166323a0f06f52c712c6796b91e9f
AyushiR0y <132144705+AyushiR0y@users.noreply.github.com>
2024-02-01 23:11:08 +0530

    if in conditionals.js

---
+++ conditionals.js
@@ -0,0 +1,9 @@
+const x=10;
+if(x==10)//== will just see value not match datatypes
+{
+    console.log('x is 10');
+}
+if(x===10)//=== checks datatypes, since both are number it is true
+{
+    console.log('x is number 10');
+}
