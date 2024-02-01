c6cffd4cfb984101f76826b380983bc87a1f6b58
AyushiR0y <132144705+AyushiR0y@users.noreply.github.com>
2024-02-01 23:35:10 +0530

    Update constructor.js

--- constructor.js
+++ constructor.js
@@ -2,8 +2,16 @@
 function Person(fname, lname, dob){
     this.fname = fname;
     this.lname =lname;
-    this.dob=dob;
+    this.dob=new Date(dob);
+    this.getFullYear= function(){
+        return this.dob.getFullYear();
+    }
+    this.getFullName = function(){
+        return `${this.fname} ${lname}`
+    }
 }
 //instantiate object
-const person1 = new Person('john','doe','1990');
-console.log(person1);
+const person1 = new Person('john','doe','4-3-1990');
+console.log(person1.getFullName());
+console.log(person1.getFullYear());
+
