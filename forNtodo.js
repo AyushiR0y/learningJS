0e3514c8d34dbd10488d9bb0fd151264267ded93
AyushiR0y <132144705+AyushiR0y@users.noreply.github.com>
2024-02-01 22:46:31 +0530

    Create forNtodo.js
    using for loop in todo array

---
+++ forNtodo.js
@@ -0,0 +1,20 @@
+const todos =[
+    {
+        id:1,
+        text: 'take trash out',
+        isCompleted: true
+    },
+    {
+        id:2,
+        text: 'homework done',
+        isCompleted: false
+    },    {
+        id:3,
+        text: 'learn js',
+        isCompleted: true
+    }
+];
+for(let todo of todos){
+    console.log(todo.text);
+}
+
